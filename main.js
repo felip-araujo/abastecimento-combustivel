ebe0fe0f71834fbabfb6413b767c8d1ec0f91cbf
felip-araujo <felipearaujoexecutive@gmail.com>
2023-10-03 16:08:57 -0400

    arquivo index

---
+++ main.js
@@ -0,0 +1,25 @@
+var tipoCombustivel = document.form.select.selectedIndex; 
+
+
+
+
+
+function mostrarSelect(){
+    alert(tipoCombustivel);
+}
+
+
+var selecao = select 
+let KmaPercorrer = 150; 
+
+function calcularValor(){
+    if(tipoCombustivel === 1){
+        resultado = KmaPercorrer * 6.59
+        console.log(resultado);
+    } else {
+        resultado = KmaPercorrer * 4.59
+        console.log(resultado);
+    }
+
+} 
+
